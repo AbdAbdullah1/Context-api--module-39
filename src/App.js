50499aa7a13287d5af41717e33f853f1addb7a94
Abd Abdullah <abdonlyone@gmail.com>
2021-03-08 08:26:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import './App.css';
 import './components/Header/Header'
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Shipment from './components/Shipment/Shipment';
+
+export const CategoryContext = createContext();
+
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div>
+    <CategoryContext.Provider value={count} >
        <p>count value: {count} </p>
       <Header count ={count} setCount={setCount}></Header>
       <Home count={count}></Home>
       <Shipment></Shipment>
-    </div>
+    </CategoryContext.Provider>
 
   );
 }
